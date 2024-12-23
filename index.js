22ba962b18ab9aec270e164806b5cfef62719100
kerbril <jibril.jlassi25000@gmail.com>
2024-12-23 19:48:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["negro negro negro", "🎮 Playing VALORANT"];
+const statusMessages = ["negro negro negro", "j'aime pas les negre"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
